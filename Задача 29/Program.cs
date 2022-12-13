3eedceae1c1613c4a318b61a5d11d426ebcd6a2f
SergeyKaraman <karamans7997@gmail.com>
2022-12-13 18:39:14 +0300

    Урок 4. Функции

---
+++ Задача 29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] num = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(0, 99);
+        Console.Write(" " + mas(i) + " ");
+    }
+
+Console.Write("]");
+
+int mas(int a)
+    {
+        return num[a];
+    }
